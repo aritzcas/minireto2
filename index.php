69e989a3d6d12b76169bd6e93e9224dc67ad6007
aritzca <aritzcas@gmail.com>
2023-01-18 20:49:40 +0100

    save

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ function connect(){
         $DB_USER=$_ENV['DB_USER'];
         $DB_PASSWORD=$_ENV['DB_PASSWORD'];
         $DB_NAME=$_ENV['DB_NAME'];
-        $dbh = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+        $DB_PORT=$_ENV['DB_PORT'];
+        $dbh = mysqli_connect("$DB_HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
         return $dbh;
     }
     catch(PDOException $e) {
